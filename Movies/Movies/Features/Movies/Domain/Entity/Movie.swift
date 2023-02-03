2458221e4eb80119fca5935e6e6db256d417abba
Ahmed MOhamed Fathy <76500072+afathe7090@users.noreply.github.com>
2023-02-04 01:58:56 +0200

    Refactor Movies model Items to only needed to create simple tests 🤝

--- Movies/Movies/Features/Movies/Domain/Entity/Movie.swift
+++ Movies/Movies/Features/Movies/Domain/Entity/Movie.swift
@@ -22,29 +22,13 @@ struct MovieModel: Codable {
 
 // MARK: - Result
 struct MovieModelData: Codable {
-    let adult: Bool?
-    let backdropPath: String?
-    let genreIDS: [Int]?
-    let id: Int?
-    let originalLanguage, originalTitle, overview: String?
-    let popularity: Double?
-    let posterPath, releaseDate, title: String?
-    let video: Bool?
-    let voteAverage: Double?
-    let voteCount: Int?
+    let overview: String?
+    let posterPath: String?
+    let title: String?
 
     enum CodingKeys: String, CodingKey {
-        case adult
-        case backdropPath = "backdrop_path"
-        case genreIDS = "genre_ids"
-        case id
-        case originalLanguage = "original_language"
-        case originalTitle = "original_title"
-        case overview, popularity
+        case overview
         case posterPath = "poster_path"
-        case releaseDate = "release_date"
-        case title, video
-        case voteAverage = "vote_average"
-        case voteCount = "vote_count"
+        case title
     }
 }
