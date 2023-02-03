ac9efe8f15a014dba041e5e1e8332ef01eb6309a
Ahmed MOhamed Fathy <76500072+afathe7090@users.noreply.github.com>
2023-02-04 01:59:51 +0200

    Create remote repository tests

---
+++ Movies/MoviesTests/MoviesRemoteRepoTests.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesRemoteRepoTests.swift
+//  MoviesTests
+//
+//  Created by Ahmed Fathy on 04/02/2023.
+//
+
+import XCTest
+@testable import Movies
+
+final class MoviesRemoteRepoTests: XCTestCase {
+    
+}
