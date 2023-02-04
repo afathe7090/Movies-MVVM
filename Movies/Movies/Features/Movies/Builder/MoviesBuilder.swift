63cfd58f3beabb2126b668603f6dda0dd8701af0
Ahmed MOhamed Fathy <76500072+afathe7090@users.noreply.github.com>
2023-02-04 02:01:08 +0200

    refactor group name to Remote Spy

--- Movies/Movies/Features/Movies/Builder/MoviesBuilder.swift
+++ Movies/Movies/Features/Movies/Builder/MoviesBuilder.swift
@@ -8,7 +8,7 @@
 import UIKit
 struct MoviesBuilder {
     private init() {}
-    static func moviesBuilder(coordinator: MoviesCoordinatorPresentable) -> UIViewController {
+    static func moviesBuilder(coordinator: MoviesCoordinatorPresentable) -> MoviesViewController {
         let remoteRepo: MoviesRepoRemoteProtocol = MoviesRepoRemoteImple()
         let repo: MoviesRepoProtocol = MoviesRepoImple(remote: remoteRepo)
         let useCase: MoviesUseCaseProtocol = MoviesUseCase(repository: repo)
